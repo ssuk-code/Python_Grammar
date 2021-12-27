753ae9529d7f2eb61f8049fff8d973858530c47f
inn-ssuk <dlstnr0721@naver.com>
2021-12-27 17:11:02 +0900

    Create project2. Baseball Game in basic

---
+++ basic/baseball_game/baseball.py
@@ -0,0 +1,61 @@
+from random import randint
+
+# 정답 숫자 3개를 뽑아 주는 generate_numbers 함수
+def generate_numbers():
+    numbers = []
+
+    while len(numbers) < 3:
+        num = randint(0, 9)
+
+        if num not in numbers:
+            numbers.append(num)
+
+    print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
+    return numbers
+
+# 유저에게 숫자 3개를 반복적으로 입력받음
+# 유저가 입력한 숫자들을 리스트에 정리해서 리턴
+def take_guess():
+    print("숫자 3개를 하나씩 차례대로 입력하세요.")
+
+    new_guess = []
+    while len(new_guess) < 3:
+        new_num = int(input("{}번째 숫자를 입력하세요: ".format(len(new_guess) + 1)))
+
+        if new_num < 0 or new_num > 9:
+            print("범위를 벗어나는 숫자입니다. 다시 입력하세요.")
+        elif new_num in new_guess:
+            print("중복되는 숫자입니다. 다시 입력하세요.")
+        else:
+            new_guess.append(new_num)
+
+    return new_guess
+
+# 스트라이크 수와 볼 수를 알려 주는 get_score 함수
+def get_score(guesses, solution):
+    strike_count = 0
+    ball_count = 0
+
+    for i in range(3):
+        if guesses[i] == solution[i]:
+            strike_count += 1
+        elif guesses[i] in solution:
+            ball_count += 1
+
+    return strike_count, ball_count
+
+# 여기서부터 게임 시작!
+ANSWER = generate_numbers()
+tries = 0
+
+while True:
+    user_guess = take_guess()
+    s, b = get_score(user_guess, ANSWER)
+
+    print("{}S {}B\n".format(s, b))
+    tries += 1
+
+    if s == 3:
+        break
+
+print("축하합니다. {}번 만에 세 숫자의 값과 위치를 모두 맞추셨습니다.".format(tries))
