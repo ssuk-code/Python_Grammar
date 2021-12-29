26a3e3fd3e4b1879c081915f5ac696499f175915
inn-ssuk <dlstnr0721@naver.com>
2021-12-30 01:23:53 +0900

    Understand the absolute path and the other path

--- intermediate/package/4. subpackage/mymath/shapes/__init__.py
+++ intermediate/package/4. subpackage/mymath/shapes/__init__.py
@@ -1,1 +1,17 @@
+# 절대 경로 임포트 : 임포트하려는 것의 경로를 다 풀어서 써 주는 것
+# 상대 경로 임포트 : 임포트를 하는 곳의 위치를 기준으로 임포트하려는 것의 위치를 상대적으로 표현
+# .은 현재 패키지 안, ..은 상위 패키지 안
+
+# 절대경로 모듈 가져오기
 from mymath.shapes import area, volume
+
+# 상대경로 모듈 가져오기
+from . import area, volume
+
+# 절대경로 모듈의 함수 가져오기
+from mymath.shapes.area import *
+from mymath.shapes.volume import *
+
+# 상대경로 모듈의 함수 가져오기
+from .area import *
+from .volume import *
--- intermediate/package/4. subpackage/mymath/shapes/area.py
+++ intermediate/package/4. subpackage/mymath/shapes/area.py
@@ -1,3 +1,10 @@
+# 절대 경로 함수 가져오기
+from mymath.stats.average import data_mean
+
+# 상대 경로 함수 가져오기
+# 상대 경로가 복잡해지는 경우 (주로 ..을 쓰는 경우)에는 그냥 절대 경로를 쓰는 것이 좋음
+from ..stats.average import data_mean
+
 PI = 3.14
 
 # 원의 면적을 구해 주는 함수
